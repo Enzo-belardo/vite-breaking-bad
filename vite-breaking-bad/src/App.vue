14c673bfbb372cc660c3027deb11078e6f58152d
Enzo-belardo <vincenzobelardo10@gmail.com>
2023-01-11 16:21:36 +0100

    create components

---
+++ vite-breaking-bad/src/App.vue
@@ -0,0 +1,24 @@
+<script>
+import appHeader from './components/appHeader.vue';
+import appMain from './components/appMain.vue';
+
+export default {
+  name: 'AppYuGiOh',
+  components: {
+    appHeader,
+    appMain,
+  }
+}
+
+</script>
+
+<template>
+  <appHeader/>
+  <appMain/>
+</template>
+
+<style lang="scss">
+@use './styles/general.scss' as * ;
+@use './styles/partials/variables' as * ;
+
+</style>
---
+++ vite-breaking-bad/src/components/appMain.vue
@@ -0,0 +1,17 @@
+<script>
+
+export default{
+
+}
+
+</script>
+
+<template>
+  
+</template>
+
+<style lang="scss">
+@use './styles/general.scss' as * ;
+@use './styles/partials/variables' as * ;
+
+</style>
