2acc74f310c011f2fc1e7a1dce0ff13b8254b841
Enzo-belardo <vincenzobelardo10@gmail.com>
2023-01-11 16:41:47 +0100

    fix: folder

--- src/components/appHeader.vue
+++ src/components/appHeader.vue
@@ -6,11 +6,14 @@ export default{
 </script>
 
 <template>
+    <header>
+        <div></div>
+    </header>
   
 </template>
 
-<style lang="scss">
-@use './styles/general.scss' as * ;
+<style lang="scss" >
+@use './styles/general/' as * ;
 @use './styles/partials/variables' as * ;
 
 </style>
--- src/components/appMain.vue
+++ src/components/appMain.vue
@@ -7,11 +7,14 @@ export default{
 </script>
 
 <template>
+    <main>
+        <div></div>
+    </main>
   
 </template>
 
 <style lang="scss">
-@use './styles/general.scss' as * ;
+@use './styles/general.scss' as* ;
 @use './styles/partials/variables' as * ;
 
 </style>
