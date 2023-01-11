efb1ad46dca7b24f139cb854371a12ffd57d1317
Enzo-belardo <vincenzobelardo10@gmail.com>
2023-01-12 00:26:00 +0100

    complete

--- src/components/appMain.vue
+++ src/components/appMain.vue
@@ -1,6 +1,7 @@
 <script>
 import CardComp from './CardComp.vue';
 import { store } from '../store.js'
+import axios from 'axios';
 
 export default{
     name: 'appMain',
@@ -11,7 +12,7 @@ export default{
     data() {
         return {
             store,
-            url: 'https://db.ygoprodeck.com/api/v7/cardinfo.php?num=10&offset=0',
+            url: 'https://db.ygoprodeck.com/api/v7/cardinfo.php?num=15&offset=0',
 
         }
     },
@@ -34,43 +35,22 @@ export default{
 </script>
 
 <template>
-    <main class="">
-        
-       <div class="container bg-light d-flex flex-wrap justify-content-center">
-          <div class="bg-dark text-light w-100 d-flex">
-              <h3 class="text-center p-2">
-                 found {{ store.cardList.length }} cards
-              </h3>
-          </div>
-
-           <div class="row row-cols-3 row-cols-md-5 g-3">
-               <div class="col-3 col-ms-5" v-for="card in store.cardList" >
-                  <div class="card">
-                     <img :src="card.card_images[0].image_url" :alt="card.name" class="img-fluid card-img-top">
-                     <div class="card-body">
-                         <h4>{{ card.name }}</h4>
-                         <p>{{ card.archetype }}</p>
-                     </div>
-                  </div>
-               </div>
+    <main>
+       <div class="container mt-4">
+          <div class="row row-cols-1 row-cols-md-3 row-cols-lg-5 g-4">
+              <CardComp v-for="n in 15"/>
            </div>
        </div>
     </main>
 </template>
 
-<style lang="scss">
+<style lang="scss" scoped>
 @use '../styles/general' as * ;
 @use '../styles/partials/variables' as * ;
 
 main{
     background-color: $main-color;
+}
 
-}
-.card-body{
-    height: 100px;
-    h4{
-        font-size: 1rem;
-    }
-}
 
 </style>
--- src/components/cardComp.vue
+++ src/components/cardComp.vue
@@ -1,19 +1,33 @@
 <script>
+import { store } from '../store.js'
 export default{
-    name: 'cardComp',
+    data() {
+        return {
+            store,
+        }
+    },
     
 }
 
 </script>
 
 <template>
-    <main>
-        <div></div>
-    </main>
+    <div class="col" v-for="card in store.cardList" >
+        <div class="card ">
+            <img :src="card.card_images[0].image_url"  class=“card-img-top” alt=““>
+            <div class="card-body">
+                <h5 class="card-title"> {{ card.name }}</h5>
+                <p class="card-text">{{ card.archetype }}</p>
+            </div>
+        </div>
+    </div>
 </template>
 
-<style lang="scss">
-@use '../styles/general' as * ;
-@use '../styles/partials/variables' as * ;
+
+
+<style lang="scss" scoped>
+.card-body{
+    height:130px;
+}
 
 </style>
